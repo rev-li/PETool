16247a3c5b4c156e540edee3f15e460287fffb0f
rev_l <rev_l@qq.com>
2023-05-24 22:23:20 +0800

    已完成文件拖拽和读取

--- newWindow.py
+++ newWindow.py
@@ -2,6 +2,7 @@ import time
 import traceback
 
 from PyQt5.QtCore import QMimeData, Qt
+from PyQt5.uic.properties import QtCore
 
 from DOSWindow import Ui_DosWindow
 from DataWindow import Ui_DataDirectoryWindow
@@ -11,20 +12,8 @@ import sys
 from PyQt5.QtWidgets import *
 
 from OptionalWindow import Ui_OptionalWindow
+from PEAnalyze import Image_Dos_Header
 from TipWindow import Ui_TipWindow
-
-
-def Operate():
-    main = Main()
-    main.show()
-    doswin = DosWin()
-    filewin = FileWin()
-    optwin = OptWin()
-    datawin = DataWin()
-    main.actionIMAGE_DOS_HEADER.triggered.connect(doswin.show)
-    main.actionIMAGE_FILE_HEADER.triggered.connect(filewin.show)
-    main.actionIMAGE_OPTIONAL_HEADER.triggered.connect(optwin.show)
-    main.actionIMAGE_DATA_HEADER.triggered.connect(datawin.show)
 
 
 class Main(QMainWindow, Ui_MainWindow):
@@ -72,14 +61,25 @@ class Main(QMainWindow, Ui_MainWindow):
                     self.content = file.read()
                     # print(self.content)
                     file.close()
-                    Operate()
+                    self.Operate(self.content)
+
+    def Operate(self, content):
+        doswin = DosWin(content)
+        filewin = FileWin()
+        optwin = OptWin()
+        datawin = DataWin()
+        self.actionIMAGE_DOS_HEADER.triggered.connect(doswin.show)
+        self.actionIMAGE_FILE_HEADER.triggered.connect(filewin.show)
+        self.actionIMAGE_OPTIONAL_HEADER.triggered.connect(optwin.show)
+        self.actionIMAGE_DATA_HEADER.triggered.connect(datawin.show)
 
 
 class DosWin(QMainWindow, Ui_DosWindow):
-    def __init__(self):
+    def __init__(self, content):
         super(DosWin, self).__init__()
         self.content = None
         self.setupUi(self)
+        self.Rewrite(content)
         self.tableWidget.clicked.connect(self.fillUserInfo)
         self.CopyButton.clicked.connect(self.copyText)
         self.CopyButton.clicked.connect(self.ToldSuccessfully)
@@ -106,6 +106,50 @@ class DosWin(QMainWindow, Ui_DosWindow):
         else:
             self.label.setText("请选择左边框中要复制的内容！！！")
         self.content = None
+
+    def Rewrite(self, content):
+        _translate = QtCore.QCoreApplication.translate
+        __sortingEnabled = self.tableWidget.isSortingEnabled()
+        self.tableWidget.setSortingEnabled(False)
+        IDH = Image_Dos_Header(content)
+        item = self.tableWidget.item(0, 0)
+        item.setText(_translate("DosWindow", IDH.e_magic))
+        item = self.tableWidget.item(1, 0)
+        item.setText(_translate("DosWindow", IDH.e_cblp))
+        item = self.tableWidget.item(2, 0)
+        item.setText(_translate("DosWindow", IDH.e_cp))
+        item = self.tableWidget.item(3, 0)
+        item.setText(_translate("DosWindow", IDH.e_crlc))
+        item = self.tableWidget.item(4, 0)
+        item.setText(_translate("DosWindow", IDH.e_cparhdr))
+        item = self.tableWidget.item(5, 0)
+        item.setText(_translate("DosWindow", IDH.e_minalloc))
+        item = self.tableWidget.item(6, 0)
+        item.setText(_translate("DosWindow", IDH.e_maxalloc))
+        item = self.tableWidget.item(7, 0)
+        item.setText(_translate("DosWindow", IDH.e_ss))
+        item = self.tableWidget.item(8, 0)
+        item.setText(_translate("DosWindow", IDH.e_sp))
+        item = self.tableWidget.item(9, 1)
+        item.setText(_translate("DosWindow", IDH.e_csum))
+        item = self.tableWidget.item(10, 0)
+        item.setText(_translate("DosWindow", IDH.e_ip))
+        item = self.tableWidget.item(11, 0)
+        item.setText(_translate("DosWindow", IDH.e_cs))
+        item = self.tableWidget.item(12, 0)
+        item.setText(_translate("DosWindow", IDH.e_lfarlc))
+        item = self.tableWidget.item(13, 0)
+        item.setText(_translate("DosWindow", IDH.e_ovno))
+        item = self.tableWidget.item(14, 0)
+        item.setText(_translate("DosWindow", IDH.e_res))
+        item = self.tableWidget.item(15, 0)
+        item.setText(_translate("DosWindow", IDH.e_oemid))
+        item = self.tableWidget.item(16, 0)
+        item.setText(_translate("DosWindow", IDH.e_res2))
+        item = self.tableWidget.item(17, 0)
+        item.setText(_translate("DosWindow", IDH.e_lfanew))
+
+        self.tableWidget.setSortingEnabled(__sortingEnabled)
 
 
 class FileWin(QMainWindow, Ui_FileWindow):
